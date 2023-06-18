0c81c7293db3356a8e8fb7e0a4e04b7eac59c1fb
Sebastian Hoffmann <sepphod@gmail.com>
2023-06-18 18:13:18 +0200

    some debug

---
+++ scripts/test_slam.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# build the workspace
+$(pwd)
+cd $(pwd)/../../..; catkin_make
+
+source devel/setup.bash
+source /opt/ros/kinetic/setup.bash
+export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/udacity_homeservicerobot/worlds/myworld.world"
+
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &    
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" & 
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" & 
+sleep 5
+xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch"  
