36bd8490d97110a8539085fc55d96b75d742cfb7
Sebastian Hoffmann <sepphod@gmail.com>
2023-06-20 08:14:25 +0200

    comment mapfile

--- catkin/src/scripts/test_navigation.sh
+++ catkin/src/scripts/test_navigation.sh
@@ -6,7 +6,7 @@ cd $(pwd)/../..; catkin_make
 source devel/setup.bash
 #source /opt/ros/kinetic/setup.bash
 export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/worlds/new_building.world"
-export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/src/map/map.yaml"
+#export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/src/map/map.yaml"
 
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &    
 sleep 5
