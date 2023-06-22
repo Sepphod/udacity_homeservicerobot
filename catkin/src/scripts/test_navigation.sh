0106782bc6a664b57d5aebb7ca2505d4a50b3ed5
Sebastian Hoffmann <sepphod@gmail.com>
2023-06-22 08:04:04 +0200

    last try

--- catkin/src/scripts/test_navigation.sh
+++ catkin/src/scripts/test_navigation.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-# build the workspace
-cd $(pwd)/../..; catkin_make
+xterm  -e "cd $(pwd)/../..;
+source devel/setup.bash;
+catkin_make;
+source devel/setup.bash;
+export ROBOT_INITIAL_POSE='-x -2.49 -y -4.28 -z 0 -R 0 -P 0 -Y 0';
+roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/worlds/new_building.world" &
 
-source devel/setup.bash
-#source /opt/ros/kinetic/setup.bash
-export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/worlds/new_building.world"
-#export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/src/map/map.yaml"
+sleep 5
 
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &    
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/src/map/map.yaml " &
+
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch" & 
-sleep 5
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch"
+
+# launch view_navigation for rviz
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
+# # build the workspace
+# cd $(pwd)/../..; catkin_make
+
+# source devel/setup.bash
+# #source /opt/ros/kinetic/setup.bash
+# export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/worlds/new_building.world"
+# #export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/src/map/map.yaml"
+
+# xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &    
+# sleep 5
+# xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch" & 
+# sleep 5
+# xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch"
--- catkin/src/scripts/test_slam.sh
+++ catkin/src/scripts/test_slam.sh
@@ -1,18 +1,58 @@
 #!/bin/bash
 
-# build the workspace
-source environment.bash
-cd $(pwd)/../..; catkin_make
+xterm  -e "cd $(pwd)/../..;
+source devel/setup.bash;
+catkin_make;
+source devel/setup.bash;
+export ROBOT_INITIAL_POSE='-x -2.49 -y -4.28 -z 0 -R 0 -P 0 -Y 0';
+roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/worlds/new_building.world" &
 
-source devel/setup.bash
-#source /opt/ros/kinetic/setup.bash
-export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/worlds/new_building.world"
-export ROBOT_INITIAL_POSE="-x -2.49 -y -4.28 -z 0"
+sleep 5
 
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &    
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+rosparam set /slam_gmapping/iterations 10;
+rosparam set /slam_gmapping/linearUpdate 0.05;
+rosparam set /slam_gmapping/angularUpdate 0.1;
+rosparam set /slam_gmapping/map_update_interval 0.25;
+rosparam set /slam_gmapping/srr 0.01;
+rosparam set /slam_gmapping/srt 0.01;
+rosparam set /slam_gmapping/str 0.02;
+rosparam set /slam_gmapping/stt 0.02;
+roslaunch turtlebot_gazebo gmapping_demo.launch" &
+
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" & 
+
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
 sleep 5
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" & 
+
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
 sleep 5
-xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch"   
+
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_teleop keyboard_teleop.launch" &
+
+# cd $(pwd)/../..; catkin_make
+
+# source devel/setup.bash
+# #source /opt/ros/kinetic/setup.bash
+# export ROBOT_INITIAL_POSE="-x -2.49 -y -4.28 -z 0 -R 0 -P 0 -Y 0"
+
+
+# xterm  -e cd $(pwd)/../..
+# source devel/setup.bash
+
+# xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/worlds/new_building.world" &    
+# sleep 5
+# xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" & 
+# sleep 5
+# xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" & 
+# sleep 5
+# xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch"   
