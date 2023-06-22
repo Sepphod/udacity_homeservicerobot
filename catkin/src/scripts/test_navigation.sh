4189dda05235b474378bcb972744fec9b411be1c
Sebastian Hoffmann <sepphod@gmail.com>
2023-06-22 18:14:29 +0200

    nav test

--- catkin/src/scripts/test_navigation.sh
+++ catkin/src/scripts/test_navigation.sh
@@ -5,13 +5,15 @@ source devel/setup.bash;
 catkin_make;
 source devel/setup.bash;
 export ROBOT_INITIAL_POSE='-x -2.49 -y -4.28 -z 0 -R 0 -P 0 -Y 0';
-roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/worlds/new_building.world" &
+export TURTLEBOT_GAZEBO_WORLD_FILE='$(pwd)/../../src/worlds/new_building.world';
+roslaunch turtlebot_gazebo turtlebot_world.launch" &
 
 sleep 5
 
 xterm -e "cd $(pwd)/../..;
 source devel/setup.bash;
-roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)../../src/map/map.yaml " &
+export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/../../src/map/map.yaml";
+roslaunch turtlebot_gazebo amcl_demo.launch" &
 
 sleep 5
 
